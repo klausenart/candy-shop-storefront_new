e790c21930ecbf6e3d45ab521ee6461fe5911951
klausenArt <122472841+klausenart@users.noreply.github.com>
2023-02-18 08:54:37 +0100

    Update routeNames.ts

--- src/constant/routeNames.ts
+++ src/constant/routeNames.ts
@@ -1,7 +1,7 @@
 export enum RouteName {
   activityView = '/activity',
   auctionsView = '/auctions',
-  home = '/home',
+  home = '/',
   marketplace = '/marketplace',
   marketplaceMint = '/marketplace/:tokenMint',
   sell = '/sell',
