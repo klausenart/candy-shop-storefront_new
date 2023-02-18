1bcca3851d1cce8f726d88aa55d4d14d129e67c8
klausenArt <122472841+klausenart@users.noreply.github.com>
2023-02-18 09:35:02 +0100

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -13,7 +13,7 @@ const Home: React.FC = () => {
         <div style={BG_Img}>
           <div style={BG_Txt}>
            <h1> Our Vision</h1>
-            <p>Klausen art Design is combining art, artificial intelligence and blockchain technology to create next level art and pushing the boundaries of imagination.</p>
+            <p>Klausen Art Design is combining art, artificial intelligence and blockchain technology to create next level art and pushing the boundaries of imagination.</p>
            </div>
            <div style ={{display:'flex', justifyContent:'center'}}>
              <img
