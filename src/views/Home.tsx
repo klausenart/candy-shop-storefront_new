c1282e3d7ddefd5ae82f263c6c2030072b944b87
klausenArt <122472841+klausenart@users.noreply.github.com>
2023-02-18 09:04:16 +0100

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -11,12 +11,15 @@ const Home: React.FC = () => {
 
       
         <div style={BG_Img}>
-          <h1> Klausenart</h1>
-            <div style ={{display:'flex', justifyContent:'center'}}>
+          <div style={BG_Txt}>
+           <h1> Our Vision</h1>
+            <p>Klausen art Design is combining art, artificial intelligence and blockchain technology to create next level art and pushing the boundaries of imagination.</p>
+           </div>
+           <div style ={{display:'flex', justifyContent:'center'}}>
              <img
               src="/Digital_Artwork_01.png"
               alt="Klausenart"
-              style={{maxWidth: '100%', height:'auto'}} />
+              style={{maxWidth: '100%', maxHeight: '600px',  height:'auto'}} />
             </div>
         </div>
     </DesContainer>
@@ -29,9 +32,3 @@ export default Home;
 const DesContainer = styled.div`
   width: 100%;
 `;
-
-const BG_Img = {
-  display:"flex",
-  justifyContent:"center",
-  alignItems:"center",
-}
