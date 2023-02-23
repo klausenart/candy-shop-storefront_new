f52470a81ee0946440ed01ecf50cba7397a545bc
klausenArt <122472841+klausenart@users.noreply.github.com>
2023-02-23 11:11:26 +0100

    Update Info.tsx

--- src/views/Info.tsx
+++ src/views/Info.tsx
@@ -11,44 +11,13 @@ const Info: React.FC = () => {
 
   return (
     <DesContainer>
-      <Stat
-        candyShop={candyShop}
-        title={'Marketplace'}
-        description={'Enable users to filter by collections and search for NFTs.'}
-        style={{ paddingBottom: 50 }}
-      />
-      <Orders
-        wallet={userWallet}
-        candyShop={candyShop}
-        walletConnectComponent={<ConnectButton />}
-        filters={FILTERS}
-        shopFilters = {false}
-        search
-        
-
-        
-      />
     </DesContainer>
   );
 };
 
 export default Info;
 
-const FILTERS = [
-  { name: 'Birdman', collectionId: '2', identifier: 1235887132 },
-  { name: 'Mr. Doodle', collectionId: '3', identifier: 1235887133 },
-];
 
 const DesContainer = styled.div`
   width: 100%;
-  .candy-filter {
-    color: #fff;
-    li:hover {
-      color: #7522f5;
-    }
-    .candy-search input {
-      padding: 10px 4px 10px 24px;
-      width: 100%;
-    }
-  }
 `;
