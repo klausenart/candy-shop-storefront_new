bb58e87481c296b09db06dbe2ec76362acf87b69
klausenArt <122472841+klausenart@users.noreply.github.com>
2023-03-23 11:57:03 +0100

    Update Tokenomics.tsx

--- src/views/Tokenomics.tsx
+++ src/views/Tokenomics.tsx
@@ -18,12 +18,12 @@ const Tokenomics: React.FC = () => {
             <b>The token address is:</b> GDzfemoYR5GkbK4YupYpyq3E8Du9fSfKXxKDpkdrqGjs 
             <a href="https://solscan.io/token/GDzfemoYR5GkbK4YupYpyq3E8Du9fSfKXxKDpkdrqGjs"> <b>(Check $KART on Solscan)</b> </a>
            </div>
-            <br>
+            <br></br>
 
             <p><b><u>Liquidity</u></b></p>
             <p>Over the last months, $KART liquidity has benn increased on Raydium LPs, with 9 million KART placed in liquidity pools. At the time of writing, the TVL is approximately 6.6k $, with KART liquidity provided by multiple SPL tokens ($USDC, $SOL, $ATLAS, $BONK, $BATT) of the Solana ecosystem.
             </p>
-            <br>
+            <br></br>
             <div style ={{display:'flex', justifyContent:'center'}}>
                         <img
                         src="/LiquidityPools.png"
@@ -31,47 +31,47 @@ const Tokenomics: React.FC = () => {
                         style={{maxWidth: '100%', maxHeight: '600px',  height:'auto'}} />
             </div>
 
-            <br>
+            <br></br>
 
             <p><b><u>Token distribution</u></b></p>
             <p>The figure shows the current $KART token distribution:</p>
 
-            &emsp;- open market (free available)<br>
-            &emsp;- community (early birds)<br>
-            &emsp;- buyback prog. (for continously increasing liquidity)<br>
-            &emsp;- burned (by hodlers)<br>
+            &emsp;- open market (free available)<br></br>
+            &emsp;- community (early birds)<br></br>
+            &emsp;- buyback prog. (for continously increasing liquidity)<br></br>
+            &emsp;- burned (by hodlers)<br></br>
 
-            <br>
+            <br></br>
             <div style ={{display:'flex', justifyContent:'center'}}>
                         <img
                         src="/KART_Distribution.jpg"
                         alt="KART_Distribution"
                         style={{maxWidth: '100%', maxHeight: '600px',  height:'auto'}} />
             </div>
-            <br>
+            <br></br>
 
             <p><b><u>Token Vesting by MeanFinance</u></b></p>
             <p>The LP tokens of the liquidity pools are put into vesting contracts by meanfinance, and are streamed back to the KART foundation wallet. That streams have been setup for 2 years, as a first step, but will be extended by new vesting streams over time, as the project is evolving. </p>
             <p>In other words: The LP tokens will be continuously emitted over the next two years to provide that no liquidity can be drawn off from the pools by the team, and therefore increasing the reputation and trust of the KART ecosystem.</p>
             <p>Adding more liquidity over time will also result in new vesting contracts to support the growth of the ecosystem. This will be defined by the team based on future strategy, as reflected in the roadmap.</p>
             <u>In summary:</u>
-            <br>
+            <br></br>
 
-            &emsp;-	Token locking: 8 Streams re-emitting Raydium LP tokens to foundation wallet<br>
-            &emsp;-	Vesting contracts: token locking is irreversible (can’t be dissolved for rug pull)<br>
-            &emsp;-	Time spawn: 2 years (ending  Q1/2025)<br>
-            &emsp;-	Emission rate: linear (LP tokens are emitted in equal parts each month)<br>
-            <br>
+            &emsp;-	Token locking: 8 Streams re-emitting Raydium LP tokens to foundation wallet<br></br>
+            &emsp;-	Vesting contracts: token locking is irreversible (can’t be dissolved for rug pull)<br></br>
+            &emsp;-	Time spawn: 2 years (ending  Q1/2025)<br></br>
+            &emsp;-	Emission rate: linear (LP tokens are emitted in equal parts each month)<br></br>
+            <br></br>
             <div style ={{display:'flex', justifyContent:'center'}}>
                         <img
                         src="/TokenStreams.png"
                         alt="Token_Streams"
                         style={{maxWidth: '100%', maxHeight: '600px',  height:'auto'}} />
             </div>
-            <br>
+            <br></br>
             <p><b><u>Note</u></b></p>
             <p>This is a very early idea of how the available KART is circulating in the ecosystem. The team is concerning of locking Team tokens successively with an expanded time range to provide a healthy tokenomics and growth of the project.</p>
-            </p>
+             
           </div>
     </DesContainer>
   );
